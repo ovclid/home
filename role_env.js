9e184c3654cb578ba16e1b742ab5a965cf155919
ovclid <53159064+ovclid@users.noreply.github.com>
2025-02-13 14:13:24 +0900

    Update role_env.js

--- role_env.js
+++ role_env.js
@@ -1,4 +1,4 @@
-fetch('https://github.com/ovclid/home/.env')
+fetch('.env')
   .then(response => response.json()) // 응답을 JSON으로 파싱
   .then(data => console.log(data))   // 파싱된 데이터를 콘솔에 출력
   .catch(error => console.error('Error:', error));
